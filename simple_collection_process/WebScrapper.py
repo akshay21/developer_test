0737b2543c060549458aa8d0435da0fd66a56e41
Akshay Gawali <akshay.jgd21@gmail.com>
2017-12-02 15:41:06 -0800

    Added error handling. Jupyter Notebook for easier implementation

--- simple_collection_process/WebScrapper.py
+++ simple_collection_process/WebScrapper.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 import  pandas as pd
 
-#specify the url
+#specifying the url
 ifp_blog="http://ifpartners.com/cut-the-wire"
 
 req =urllib2.Request(ifp_blog, headers={'User-Agent' : " Magic Browser"})
@@ -17,11 +17,14 @@ soup = BeautifulSoup(page, 'html.parser')
 
 data=[]
 
+#collecting images and articles in list
 section=soup.find('section')
+
 imgs= soup.find_all('img', attrs={'class': "attachment-post-thumbnail size-post-thumbnail wp-post-image"})
 
-
 articles=soup.find_all('article')
+
+#looping over the list to extract desired details: Title, Updated Time, Author, Content and Banner href
 for i in range(0,6):
     blog = []
     article= articles[i]
@@ -33,7 +36,7 @@ for i in range(0,6):
     blog.extend((title, updateTime,author,content,bannerImg))
     data.append(blog)
 
-
+#exporting the data to csv
 df = pd.DataFrame(data, columns=["Title","Author","Updated Time","Content", "Image href"])
 df.to_csv('ofile.csv', index=False)
 
