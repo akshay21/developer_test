48d83eea0843a48bb25e59c8a82d90e1660d1b34
Akshay Gawali <akshay.jgd21@gmail.com>
2017-12-03 20:47:58 -0800

    Extra Credit part added. Adding HowToUse.txt instructions. Few changes and refactoring.

--- simple_collection_process/WebScrapper.py
+++ simple_collection_process/WebScrapper.py
@@ -1,17 +1,23 @@
-#import libraries
+#!/usr/bin/env python
 
+#import libraries
 import  urllib2
 
 from bs4 import BeautifulSoup
 
 import  pandas as pd
 
+import requests
+
 #specifying the url
 ifp_blog="http://ifpartners.com/cut-the-wire"
 
 req =urllib2.Request(ifp_blog, headers={'User-Agent' : " Magic Browser"})
 
-page = urllib2.urlopen(req)
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e
 
 soup = BeautifulSoup(page, 'html.parser')
 
@@ -23,6 +29,17 @@ section=soup.find('section')
 imgs= soup.find_all('img', attrs={'class': "attachment-post-thumbnail size-post-thumbnail wp-post-image"})
 
 articles=soup.find_all('article')
+
+#Extra Credit: Download the first image and save to local file system
+img_links = [each.get('src') for each in imgs]
+image = img_links[0] #link to the first image in the content
+filename = image.split('/')[-1]
+
+try:
+    with open(filename,'wb' ) as imgFile:
+        imgFile.write(requests.get(image).content)
+except :
+    print "Error opening file"
 
 #looping over the list to extract desired details: Title, Updated Time, Author, Content and Banner href
 for i in range(0,6):
